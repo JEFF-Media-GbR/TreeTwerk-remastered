c6e6ab79208b8b541778e2bc5299573722b2ae67
Cazcez <50046813+Cazcez@users.noreply.github.com>
2020-06-15 06:44:08 +0300

    optimized

--- src/treetwerk/events/Sneakevent.java
+++ src/treetwerk/events/Sneakevent.java
@@ -1,8 +1,8 @@
 package treetwerk.events;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Tag;
 import org.bukkit.TreeType;
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
-import org.bukkit.material.Sapling;
 
 public class Sneakevent implements Listener 
 {
@@ -21,12 +20,20 @@ public class Sneakevent implements Listener
 	@EventHandler
 	public void PlayerEvent(PlayerToggleSneakEvent e) 
 	{
-		getNearbySaplings(e.getPlayer());
+		ArrayList<Block> blocks = getNearbySaplings(e.getPlayer());
+		
+		for (Block block : blocks)
+		{
+			TreeType type = CheckTreeType(block);	 
+			GrowTree(block, type);
+		}
+
 	}
 	
-    private void getNearbySaplings(Player player) 
+    private ArrayList<Block> getNearbySaplings(Player player) 
     {
-        int range = 5;
+    	ArrayList<Block> Br = new ArrayList< Block>();
+    	int range = 5;
         for (int x = player.getLocation().getBlockX() - range ; x <= player.getLocation().getBlockX() + range; x++) 
         {
             for (int y = player.getLocation().getBlockY() - range ; y <= player.getLocation().getBlockY() + range; y++)
@@ -36,62 +43,71 @@ public class Sneakevent implements Listener
                     Block block = player.getWorld().getBlockAt(x, y, z);
                     if (Tag.SAPLINGS.isTagged(block.getType())) 
                     {
-                    	if (!TwerkCount.containsKey(block))
-                    		TwerkCount.put(block, 0);
-                    	
-                    	int newtwerk = TwerkCount.get(block) + 1; 
-                    	if (newtwerk >= 5)
-                    	{
-                            TreeType type = null;
-                            
-                            if (block.getType().equals(Material.OAK_SAPLING))
-                                type = TreeType.TREE;
-                            
-                            if (block.getType().equals(Material.SPRUCE_SAPLING))
-                            	type = TreeType.REDWOOD;
-                            
-                            
-                            if (block.getType().equals(Material.JUNGLE_SAPLING))
-                                type = TreeType.SMALL_JUNGLE;
-                            
-                            
-                            if (block.getType().equals(Material.BIRCH_SAPLING))
-                                type = TreeType.BIRCH;
-                            
-                            
-                            if (block.getType().equals(Material.ACACIA_SAPLING))
-                                type = TreeType.ACACIA;
-                            
-                            block.setType(Material.AIR);
-                            block.getWorld().generateTree(block.getLocation(), type); 
-                            TwerkCount.remove(block);
-
-                            if(block.getType().equals(Material.AIR))
-                            {
-                            	if (type.equals(TreeType.TREE))
-                            		block.setType(Material.OAK_SAPLING);
-                            		
-                            		
-                            	if (type.equals(TreeType.ACACIA))
-                            		block.setType(Material.ACACIA_SAPLING);
-                            	
-                            	
-                            	else if (type.equals(TreeType.JUNGLE))
-                            		block.setType(Material.JUNGLE_SAPLING);
-                            	
-                            	
-                            	if(type.equals(TreeType.BIRCH))
-                            		block.setType(Material.BIRCH_SAPLING);
-                            }
-                    	}
-                    	else
-                    		TwerkCount.put(block, newtwerk);
-                    	
-
+                    	Br.add(block);     
                     }
                 }
             }
         }
+        return Br;
 
+    }
+    
+    private TreeType CheckTreeType(Block block) 
+    {
+        TreeType type = null;
+        
+        if (block.getType().equals(Material.OAK_SAPLING))
+            type = TreeType.TREE;
+        
+        if (block.getType().equals(Material.SPRUCE_SAPLING))
+        	type = TreeType.REDWOOD;
+        
+        
+        if (block.getType().equals(Material.JUNGLE_SAPLING))
+            type = TreeType.SMALL_JUNGLE;
+        
+        
+        if (block.getType().equals(Material.BIRCH_SAPLING))
+            type = TreeType.BIRCH;
+        
+        
+        if (block.getType().equals(Material.ACACIA_SAPLING))
+            type = TreeType.ACACIA;
+        
+        return type;
+    }
+    
+    private void GrowTree(Block block, TreeType type)
+    {
+    	if (!TwerkCount.containsKey(block))
+    		TwerkCount.put(block, 0);
+    	
+    	int newtwerk = TwerkCount.get(block) + 1; 
+    	if (newtwerk >= 5)
+    	{
+            block.setType(Material.AIR);
+            block.getWorld().generateTree(block.getLocation(), type); 
+            TwerkCount.remove(block);
+
+            if(block.getType().equals(Material.AIR))
+            {
+            	if (type.equals(TreeType.TREE))
+            		block.setType(Material.OAK_SAPLING);
+            		
+            		
+            	if (type.equals(TreeType.ACACIA))
+            		block.setType(Material.ACACIA_SAPLING);
+            	
+            	
+            	else if (type.equals(TreeType.JUNGLE))
+            		block.setType(Material.JUNGLE_SAPLING);
+            	
+            	
+            	if(type.equals(TreeType.BIRCH))
+            		block.setType(Material.BIRCH_SAPLING);
+            }
+    	}
+    	else
+    		TwerkCount.put(block, newtwerk);    	
     }
 }
