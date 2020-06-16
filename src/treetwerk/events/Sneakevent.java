82a09cd0aa2dc08b013d935c38d619c31a0b06a2
Cazcez <50046813+Cazcez@users.noreply.github.com>
2020-06-16 06:29:33 +0300

    Mushroom trees and grow particle added

--- src/treetwerk/events/Sneakevent.java
+++ src/treetwerk/events/Sneakevent.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Tag;
 import org.bukkit.TreeType;
 import org.bukkit.block.Block;
@@ -21,12 +22,44 @@ public class Sneakevent implements Listener
 	@EventHandler
 	public void PlayerEvent(PlayerToggleSneakEvent e) 
 	{
+		boolean configForTree = false;
+		if (!(e.isSneaking()))
+			return;
+		
 		ArrayList<Block> blocks = getNearbySaplings(e.getPlayer());
 		
 		for (Block block : blocks)
 		{
-			TreeType type = CheckTreeType(block);	 
-			GrowTree(block, type);
+			TreeType type = CheckTreeType(block);
+
+			
+	        if (block.getType().equals(Material.OAK_SAPLING))
+	            configForTree = ConfigFolder.TwerkConfig2.get("OakTree");
+	        
+	        if (block.getType().equals(Material.SPRUCE_SAPLING))
+	            configForTree = ConfigFolder.TwerkConfig2.get("SpruceTree");
+	        
+	        
+	        if (block.getType().equals(Material.JUNGLE_SAPLING))
+	            configForTree = ConfigFolder.TwerkConfig2.get("JungleTree");
+	        
+	        
+	        if (block.getType().equals(Material.BIRCH_SAPLING))
+	            configForTree = ConfigFolder.TwerkConfig2.get("BirchTree");
+	                
+	        if (block.getType().equals(Material.ACACIA_SAPLING))
+	            configForTree = ConfigFolder.TwerkConfig2.get("AcaciaTree");
+	        
+	        if (block.getType().equals(Material.RED_MUSHROOM))
+	            configForTree = ConfigFolder.TwerkConfig2.get("RedMushroomTree");
+	        
+	        	
+	        if (block.getType().equals(Material.BROWN_MUSHROOM))
+	            configForTree = ConfigFolder.TwerkConfig2.get("BrownMushroomTree");
+			
+			if (configForTree == true)
+				GrowTree(block, type, e.getPlayer());
+			
 		}
 
 	}
@@ -34,7 +67,7 @@ public class Sneakevent implements Listener
     private ArrayList<Block> getNearbySaplings(Player player) 
     {
     	ArrayList<Block> Br = new ArrayList< Block>();
-    	int range = 5;
+    	int range = ConfigFolder.TwerkConfig.get("RangeForShifting");
         for (int x = player.getLocation().getBlockX() - range ; x <= player.getLocation().getBlockX() + range; x++) 
         {
             for (int y = player.getLocation().getBlockY() - range ; y <= player.getLocation().getBlockY() + range; y++)
@@ -42,7 +75,7 @@ public class Sneakevent implements Listener
                 for (int z = player.getLocation().getBlockZ() - range ; z <= player.getLocation().getBlockZ() + range; z++)
                 {
                     Block block = player.getWorld().getBlockAt(x, y, z);
-                    if (Tag.SAPLINGS.isTagged(block.getType())) 
+                    if (Tag.SAPLINGS.isTagged(block.getType()) || block.getType().equals(Material.BROWN_MUSHROOM) || block.getType().equals(Material.RED_MUSHROOM)) 
                     {
                     	Br.add(block);     
                     }
@@ -75,10 +108,17 @@ public class Sneakevent implements Listener
         if (block.getType().equals(Material.ACACIA_SAPLING))
             type = TreeType.ACACIA;
         
+        if (block.getType().equals(Material.RED_MUSHROOM))
+            type = TreeType.RED_MUSHROOM;
+        
+        	
+        if (block.getType().equals(Material.BROWN_MUSHROOM))
+            type = TreeType.BROWN_MUSHROOM;
+
         return type;
     }
     
-    private void GrowTree(Block block, TreeType type)
+    private void GrowTree(Block block, TreeType type, Player player)
     {
     	if (!TwerkCount.containsKey(block))
     		TwerkCount.put(block, 0);
@@ -95,20 +135,28 @@ public class Sneakevent implements Listener
             	if (type.equals(TreeType.TREE))
             		block.setType(Material.OAK_SAPLING);
             		
-            		
             	if (type.equals(TreeType.ACACIA))
             		block.setType(Material.ACACIA_SAPLING);
-            	
-            	
+            	       	
             	else if (type.equals(TreeType.JUNGLE))
             		block.setType(Material.JUNGLE_SAPLING);
             	
-            	
             	if(type.equals(TreeType.BIRCH))
             		block.setType(Material.BIRCH_SAPLING);
+            	
+            	if (type.equals(TreeType.BROWN_MUSHROOM));
+        			block.setType(Material.BROWN_MUSHROOM);
+        			
+                if (type.equals(TreeType.RED_MUSHROOM));
+            		block.setType(Material.RED_MUSHROOM);        			
+            	
             }
     	}
     	else
-    		TwerkCount.put(block, newtwerk);    	
+    		TwerkCount.put(block, newtwerk); 
+    	
+    	if (ConfigFolder.TwerkConfig2.get("GrowingParticle").equals(true))
+    		player.spawnParticle(Particle.SPELL, block.getLocation(), 20, 1.2D, 0D, 1.2D);
+    	
     }
 }
